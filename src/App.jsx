8debc917911c6655405b68625bbc505f2fde0f83
bader zakaria <baderabdzakaria@gmail.com>
2023-05-17 07:16:15 +0300

    hot fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Values/Value";
 import Contact from "./components/Contact/Contact";
-import GetStarted from "./components/GetSarted/GetStarted";
 import Footer from "./components/Footer/Footer";
 
 /**
@@ -26,7 +25,6 @@ function App() {
       <Residencies />
       <Value />
       <Contact />
-      {/* <GetStarted /> */}
       <Footer />
     </div>
   );
